50bc610e100c321554143ff34d389faf2bbc63b0
alborzk <khakbaza@myumanitoba.ca>
2022-10-11 06:07:39 -0500

    Hotfix 2

--- containers/Recommendations.jsx
+++ containers/Recommendations.jsx
@@ -8,7 +8,7 @@ const Recommendations = () => {
 	return recommendations && (
 		<section className="section pb-100 bg-primary">
 			<Container className="text-center">	
-			<Fade left duration={2000}><h1 className="text-white my-5 display-3"  id="section-title"><strong>{"Things I Recommend"}</strong></h1></Fade>
+			<Fade left duration={2000}><h1 className="text-white my-5 display-3"  id="section-title"><strong>{"You Might Like"}</strong></h1></Fade>
 				<Row className="row-grid align-items-start">
 					{recommendations.map((data, i) => {
 						return <RecommendCard key={i} data={data} />;
